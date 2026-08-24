6bda428fcf3addf506b0b985db6ed76d4c315bbb
kelver-web <kelverwt@gmail.com>
2026-08-24 19:55:31 -0300

    inicialização, configuração do projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold text-red-600">
+      Sistema OS — Frontend funcionando
+    </h1>
+  )
+}
+
+export default App
