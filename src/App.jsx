e3bfd0987ed7dc89f1527d6419ca3b34db9d8737
kelver-web <kelverwt@gmail.com>
2026-08-26 19:52:39 -0300

    feat: configura cliente axios com variavel de ambiente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { useEffect } from 'react';
+
+import api from './services/api';
+
 
 function App() {
 
+  useEffect(() => {
+    api.get('/clients/')
+    .then(r => {
+      r.console.log('Sucesso: ', r.data)
+    })
+    .catch(e => {
+      console.log('Erro: ', e.response?.data)
+    })
+  }, [])
+
   return (
-    <h1 className="text-3xl font-bold text-red-600">
-      Sistema OS — Frontend funcionando
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold text-blue-600">Sistema OS</h1>
+    </>
   )
 }
 
